98ababb1e607aeba97ab9b6bb05eb84f3dedbf25
ol222hf <ol222hf@student.lnu.se>
2022-01-31 19:13:11 +0100

    Comments

--- scraper/src/app.ts
+++ scraper/src/app.ts
@@ -1,6 +1,7 @@
 import WikiPageScraper from "./WikiPageScraper";
 import { createFile, createDir } from "./FileCreator";
 
+/** Runs the scraper application. */
 const run = async () => {
   const mainScraper = new WikiPageScraper(
     "https://en.wikipedia.org/wiki/Marvel_Cinematic_Universe"
@@ -8,19 +9,23 @@ const run = async () => {
 
   await mainScraper.run();
 
+  // Create directories
   console.time("CreateDir");
   createDir("../data/Words/Marvel");
   createDir("../data/Links/Marvel");
   console.timeEnd("CreateDir");
 
   console.time("CreateFiles");
+  // Loop links found
   for await (const link of mainScraper.links) {
     const subScraper = new WikiPageScraper("https://en.wikipedia.org" + link);
     await subScraper.run();
 
+    // File names
     const wordsPath = "../data/Words/Marvel/" + link.replace("/wiki/", "");
     const linksPath = "../data/Links/Marvel/" + link.replace("/wiki/", "");
 
+    // Create files using data retreived in scraper
     createFile(wordsPath, subScraper.words.join(" "));
     createFile(linksPath, subScraper.links.join("\n"));
   }
