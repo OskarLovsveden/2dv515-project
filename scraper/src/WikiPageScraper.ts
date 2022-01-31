32141bb86612560880765ae56ab9023a30d93efe
ol222hf <ol222hf@student.lnu.se>
2022-01-31 19:19:49 +0100

    method scraping wiki page for words and links

--- scraper/src/WikiPageScraper.ts
+++ scraper/src/WikiPageScraper.ts
@@ -1,3 +1,6 @@
+import * as cheerio from "cheerio";
+import fetch from "node-fetch";
+
 class WikiPageScraper {
   private _url: string;
   private _links: Set<string>;
@@ -16,6 +19,42 @@ class WikiPageScraper {
   get words(): string[] {
     return this._words;
   }
+
+  run = async () => {
+    const res = await fetch(this._url);
+    const body = await res.text();
+    const $ = cheerio.load(body);
+    const links = $("#bodyContent a");
+    const words = $("#bodyContent p");
+
+    links.each((i: number, link: cheerio.Element) => {
+      const href = $(link).attr("href");
+      if (href && this.isValid(href)) {
+        this._links.add(href);
+      }
+    });
+
+    words.each((i: number, word: cheerio.Element) => {
+      let text = $(word).text();
+      if (text) {
+        // Remove all commas and periods.
+        text = text.replace(/[,|.]/g, "");
+        // Remove all references, example: [123]
+        text = text.replace(/(\[[0-9]+\])/g, "");
+        // Remove all non-digits, non-letters and non-white-spaces
+        text = text.replace(/[^\d|\w|\s]/g, " ");
+        text = text.toLowerCase();
+        text = text.trim();
+
+        console.log(text);
+
+        if (text.length) {
+          // Add text as single words, separated by white-space
+          this._words = [...this._words, ...text.split(/\s+/g)];
+        }
+      }
+    });
+  };
 
   /**
    * Checks whether a href is valid or not.
